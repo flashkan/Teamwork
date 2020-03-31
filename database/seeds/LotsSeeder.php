d5f4ef1c8f2b8eea10e7f64ebedf1ebd711facda
flashkan <nikol-babkin@yandex.ru>
2020-03-31 17:48:23 +0300

    add seller and buyback price to table lots

--- database/seeds/LotsSeeder.php
+++ database/seeds/LotsSeeder.php
@@ -22,11 +22,12 @@ class LotsSeeder extends Seeder
         for ($i = 1; $i <= 20; $i++) {
             $data[] = [
                 'product_id' => $faker->numberBetween(1, 10),
-                'price' => $faker->randomFloat(2, 1000, 5000),
-                'end_time' =>
-                    date('Y-m-d H:i:s', time() + 3600 * 3 + rand(1, 60) * rand(1, 60) * rand(10, 50)),
+                'seller_id' => $faker->numberBetween(1, 10),
+                'start_price' => $faker->randomFloat(2, 1000, 5000),
+                'buyback_price' => $faker->randomFloat(2, 50000, 100000),
+                'end_time' => $faker->dateTimeBetween($startDate = '3 hour', $endDate = '5 days', $timezone = 'MSK'),
                 'current_rate' => $faker->randomFloat(2, 5000, 50000),
-                'current_buyer_id' => $faker->numberBetween(6, 10),
+                'current_buyer_id' => $faker->numberBetween(1, 10),
             ];
         }
         return $data;
