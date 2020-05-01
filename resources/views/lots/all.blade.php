cca08343299482c87e26a9d0ad7241e4ac63f3b2
AntePost <is1213791@gmail.com>
2020-05-01 22:58:12 +0300

    Add better random to carousel and make images identical size

--- resources/views/lots/all.blade.php
+++ resources/views/lots/all.blade.php
@@ -7,7 +7,7 @@
             @foreach ($lots as $lot)
                 <div class="card col-4 p-1" style="width: 18rem;">
                     <div class="card-body">
-                        <img class="card-img p-0 my-2" src="{{ $lot->product()->img_url
+                        <img class="card-img p-0 my-2 lot-in-all-lots" src="{{ $lot->product()->img_url
 ? Storage::url($lot->product()->img_url) : Storage::url('placeholder.jpg') }}" alt="img_product">
                         <h4 class="card-text"><strong>Product:</strong> {{ $lot->product()->name }}</h4>
                         <h5 class="card-title"><strong>Start price:</strong> {{ $lot->start_price }}</h5>
